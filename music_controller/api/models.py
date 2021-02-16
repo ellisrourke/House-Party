57d0e51b86333441bc8194d6ad90edadcc5af17d
ellisrourke <ellisrourke@gmail.com>
2021-02-16 16:54:58 +1000

    init and setup room api

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,21 @@
+import string
+import random
 from django.db import models
 
 # Create your models here.
+
+def generate_room_id():
+    length = 8
+    while True:
+        room_id = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=room_id).count() == 0:
+            break
+    return room_id
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
